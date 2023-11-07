4cae881feba5d039042d75290fa350532f3e3ea0
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-08 00:40:41 +0530

    Code_updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
-  
-  let counter = 5
+  let counter = 15;
+  const addValue = () => {
+    console.log("value added", Math.random());
+  }
   return (
     <>
      <h1> Hello React</h1>
      <h2> Counter Value: {counter}</h2>
 
-     <button> Add value</button>
+     <button
+     onClick={addValue}
+     > Add value</button>
      <br/>
 
      <button>Remove value</button>
